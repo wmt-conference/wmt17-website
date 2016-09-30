84080665aa920e285665dff91e5467fd1d6de200
Barry Haddow <barry.haddow@gmail.com>
2016-09-30 10:06:10 +0100

    stub site

---
+++ menu.js
@@ -0,0 +1,22 @@
+document.write('\
+[<A HREF="/wmt17/index.html">HOME</A>] \
+[<A HREF="/wmt17/index.html">SCHEDULE</A>] \
+[<A HREF="/wmt17/index.html">PAPERS</A>] \
+[<A HREF="/wmt17/index.html">AUTHORS</A>] \
+[<A HREF="/wmt17/index.html">RESULTS</A>] <br>  \
+TRANSLATION TASKS: \
+  [<A HREF="/wmt17/translation-task.html">NEWS</A>] \
+  [<A HREF="/wmt17/biomedical-translation-task.html">BIOMEDICAL</A>] \
+  [<A HREF="/wmt17/multimodal-task.html">MULTIMODAL</A>] \
+  [<A HREF="/wmt17/pronoun-task.html">PRONOUN</A>] \
+<br> EVALUATION TASKS: \
+  [<A HREF="/wmt17/metrics-task">METRICS</A>] \
+  [<A HREF="/wmt17/quality-estimation-task.html">QUALITY ESTIMATION</A>]   \
+<br> OTHER TASKS: \
+  [<A HREF="/wmt17/ape-task.html">AUTOMATIC POST-EDITING</A>] \
+  [<A HREF="/wmt17/bilingual-task.html">BILINGUAL DOCUMENT ALIGNMENT</A>] \
+');
+/*| [<A HREF="/wmt17/papers.html">PAPERS</A>]  \
+| [<A HREF="/wmt17/authors.html">AUTHORS</A>] \
+| [<A HREF="/wmt17/results.html">RESULTS</A>]\ */
+
---
+++ title.js
@@ -0,0 +1,11 @@
+document.write('\
+      <h2> \
+        TBD 2017 <br> \
+        SECOND CONFERENCE ON<br> \
+        MACHINE TRANSLATION (WMT17) \
+      </h2> \
+      <h3> \
+        TBD, 2017<br> \
+        TBD \
+      </h3>\
+');
