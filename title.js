e5d082a6a59f3cd7c16483138ba1f4306bb071b5
Barry Haddow <barry.haddow@gmail.com>
2016-11-23 12:26:42 +0000

    news task, dates, location

--- title.js
+++ title.js
@@ -1,11 +1,11 @@
 document.write('\
       <h2> \
-        TBD 2017 <br> \
+        EMNLP 2017 <br> \
         SECOND CONFERENCE ON<br> \
         MACHINE TRANSLATION (WMT17) \
       </h2> \
       <h3> \
-        TBD, 2017<br> \
-        TBD \
+        September, 2017<br> \
+        Copenhagen, Denmark \
       </h3>\
 ');
