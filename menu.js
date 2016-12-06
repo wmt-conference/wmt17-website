18a243a68aacf9ae298d21194facc30b8fc9a186
Barry Haddow <barry.haddow@gmail.com>
2016-12-06 22:05:59 +0000

    blanks in menu

--- menu.js
+++ menu.js
@@ -5,16 +5,16 @@ document.write('\
 [<A HREF="/wmt17/index.html">AUTHORS</A>] \
 [<A HREF="/wmt17/index.html">RESULTS</A>] <br>  \
 TRANSLATION TASKS: \
-  [<A HREF="/wmt17/translation-task.html">NEWS</A>] \
-  [<A HREF="/wmt17/biomedical-translation-task.html">BIOMEDICAL</A>] \
-  [<A HREF="/wmt17/multimodal-task.html">MULTIMODAL</A>] \
+  [<A HREF="/wmt17/index.html">NEWS</A>] \
+  [<A HREF="/wmt17/index.html">BIOMEDICAL</A>] \
+  [<A HREF="/wmt17/index.html">MULTIMODAL</A>] \
 <br> EVALUATION TASKS: \
-  [<A HREF="/wmt17/metrics-task">METRICS</A>] \
-  [<A HREF="/wmt17/quality-estimation-task.html">QUALITY ESTIMATION</A>]   \
+  [<A HREF="/wmt17/index.html">METRICS</A>] \
+  [<A HREF="/wmt17/index.html">QUALITY ESTIMATION</A>]   \
 <br> OTHER TASKS: \
-  [<A HREF="/wmt17/ape-task.html">AUTOMATIC POST-EDITING</A>] \
-  [<A HREF="/wmt17/bilingual-task.html">BILINGUAL DOCUMENT ALIGNMENT</A>] \
-  [<A HREF="/wmt17/bandit-learning-task.html">BANDIT LEARNING</A>] \
+  [<A HREF="/wmt17/index.html">AUTOMATIC POST-EDITING</A>] \
+  [<A HREF="/wmt17/index.html">NEURAL MT TRAINING</A>] \
+  [<A HREF="/wmt17/index.html">BANDIT LEARNING</A>] \
 ');
 /*| [<A HREF="/wmt17/papers.html">PAPERS</A>]  \
 | [<A HREF="/wmt17/authors.html">AUTHORS</A>] \
