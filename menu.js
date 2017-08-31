411a9aabfc5b00acfe2d0ce531fea5cda7132626
www-data <www-data@vali.inf.ed.ac.uk>
2017-08-31 11:07:44 +0000

    papers

--- menu.js
+++ menu.js
@@ -1,5 +1,8 @@
 document.write('\
-[<A HREF="/wmt17/index.html">HOME</A>] \
+  [<A HREF="/wmt17/index.html">HOME</A>] \
+  [<A HREF="/wmt17/papers.html">PAPERS</A>]  \
+  [<A HREF="/wmt17/program.html">SCHEDULE</A>]  \
+  [<A HREF="/wmt17/authors.html">AUTHORS</A>] \
  <br>  \
 TRANSLATION TASKS: \
   [<A HREF="/wmt17/translation-task.html">NEWS</A>] \
@@ -13,11 +16,9 @@ TRANSLATION TASKS: \
   [<A HREF="/wmt17/nmt-training-task">NEURAL MT TRAINING</A>] \
   [<A HREF="/wmt17/bandit-learning-task.html">BANDIT LEARNING</A>] \
 ');
-/*| [<A HREF="/wmt17/papers.html">PAPERS</A>]  \
-| [<A HREF="/wmt17/authors.html">AUTHORS</A>] \
-| [<A HREF="/wmt17/results.html">RESULTS</A>]\ */
 
 /*[<A HREF="/wmt17/index.html">SCHEDULE</A>] \
+  [<A HREF="/wmt17/results.html">RESULTS</A>]\
 [<A HREF="/wmt17/index.html">PAPERS</A>] \
 [<A HREF="/wmt17/index.html">AUTHORS</A>] \
 [<A HREF="/wmt17/index.html">RESULTS</A>] */
